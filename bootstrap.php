9b824a6641a563fe7352b5b6949c2a0105c82cd4
arielalvesdutra <alves.lsn@gmail.com>
2019-03-16 14:28:11 -0300

    <feat>(bootstrap.php): - Adiciona métodos debug() e debugd() - Adiciona método setOnExecutionTimeEnvFileValues() para ler os valores do arquivo .env e colocar na execução - Adiciona método para criação, caso não exista, da base de dados setada no arquivo .env ao iniciar o sistema - Adiciona método para criação, caso não exista, do tabela users ao iniciar o sistema

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,64 @@
 <?php
 
 require 'vendor/autoload.php';
+
+/**
+ * Método de Depuração
+ */
+function debug()
+{
+    $arguments = func_get_args();
+    echo '<pre>';
+    foreach($arguments as &$argument){
+        if (is_object($argument) || is_array($argument)) {
+            print_r($argument);
+        } elseif (empty($argument) || is_resource($argument)) {
+            var_dump($argument);
+        } else {
+            echo (string)$argument;
+        }
+    }
+    echo '</pre>';
+}
+
+/**
+ * Método de Depuração que encerra execução do código
+ */
+function debugd()
+{
+    $arguments = func_get_args();
+    echo '<pre>';
+    foreach($arguments as &$argument){
+        if (is_object($argument) || is_array($argument)) {
+            print_r($argument);
+        } elseif (empty($argument) || is_resource($argument)) {
+            var_dump($argument);
+        } else {
+            echo (string)$argument;
+        }
+    }
+    echo '</pre>';
+    die();
+}
+
+/**
+ * Método para setar os valores do arquivo .env na execução do PHP
+ */
+function setOnExecutionTimeEnvFileValues()
+{
+
+    $envFileValues = parse_ini_file('.env');
+
+    foreach ($envFileValues as $key => $envFileValue) {
+        $_ENV[$key] = $envFileValue;
+    }
+}
+
+setOnExecutionTimeEnvFileValues();
+
+use App\Databases\Factories\Databases\HourAdjustmentSystemDatabase;
+use App\Databases\Factories\Tables\UserTable;
+
+HourAdjustmentSystemDatabase::create();
+UserTable::create();
+
