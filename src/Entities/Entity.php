f05968c8341173dd158e8441caafa94b66fc4eb6
arielalvesdutra <alves.lsn@gmail.com>
2019-03-16 14:24:51 -0300

    <feat>(src/Entities/Entity): - Adiciona parametro opcional $id no constrututor da entidade - Adiciona métodos setId() e get()

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -12,4 +12,31 @@ abstract class Entity
      * @var int $id
      */
     protected $id;
+
+    /**
+     * Entity constructor.
+     * @param int $id
+     */
+    public function __construct(int $id = null)
+    {
+        if (!empty($id)) {
+            $this->setId($id);
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    private function setId(int $id): void
+    {
+        $this->id = $id;
+    }
 }
