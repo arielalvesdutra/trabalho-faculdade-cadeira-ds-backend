6d47fabacbb081411a55cac90f5700492ecc34cd
arielalvesdutra <alves.lsn@gmail.com>
2019-03-17 11:54:24 -0300

    <feat>(tests/Factories/Entitites/UserEntityFactoryTest): - Adiciona classe de Testes Unitários para a factory de entidade de usuário

---
+++ tests/Factories/Entities/UserEntityFactoryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Factories\Entities;
+
+use App\Factories\Entities\UserEntityFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ *
+ * Class UserEntityFactoryTest
+ * @package Tests\Factories\Entities
+ */
+class UserEntityFactoryTest extends TestCase
+{
+    /**
+     * Verifica se a classe existe
+     *
+     * Verifica se a classe extende App\Factories\Entities\EntityFactory
+     */
+    protected function assertPreConditions(): void
+    {
+        $this->assertTrue(
+            class_exists('App\Factories\Entities\UserEntityFactory'),
+            'Classe App\Factories\Entities\UserEntityFactory não encontrada.'
+        );
+    }
+
+
+    /**
+     * Testa o método estático create()
+     */
+    public function testCreateUserEntityShouldWork()
+    {
+
+        $userName = 'teste factory';
+        $userEmail = "teste.factory@teste.com";
+        $userPassword = '123456';
+
+        $userEntity = UserEntityFactory::create(
+          $userName,
+          $userEmail ,
+          $userPassword
+        );
+
+        $this->assertEquals(
+          $userName,
+          $userEntity->getName()
+        );
+
+        $this->assertEquals(
+            $userEmail,
+            $userEntity->getEmail()
+        );
+
+        $this->assertEquals(
+            $userPassword,
+            $userEntity->getPassword()
+        );
+    }
+}
