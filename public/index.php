3c50bd48a50de478941598806163759b4cf92bd4
arielalvesdutra <alves.lsn@gmail.com>
2019-03-17 19:29:34 -0300

    <feat>(public/index.php): - Adiciona controller App\Controllers\User e injeta as dependências dela através do Container do Slim - Adiciona a rota POST - /users - Controllers\User:create

--- public/index.php
+++ public/index.php
@@ -2,16 +2,49 @@
 
 require '../bootstrap.php';
 
+use App\Controllers;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\App;
+use Slim\Container;
 
-$slim = new App();
+/**
+ * Containers
+ */
+$container = new Container();
+/**
+ * Injeta as dependências da controller User
+ *
+ * @return Controllers\User
+ */
+$container[Controllers\User::class] = function ()
+{
+  return new \App\Controllers\User(
 
+      new \App\Repositories\User(
+        new  \App\Models\User(
+            \App\Databases\Factories\Connections\DefaultDatabaseConnection::getConnection()
+        )
+      )
+  );
+};
+
+/**
+ * Instancia o slim
+ */
+$slim = new App($container);
+
+/**
+ * Rotas
+ */
 
 $slim->get('/', function (RequestInterface $request, ResponseInterface $response){
     return $response->withJson("Primeira rota.");
 });
 
+/**
+ * Users
+ */
+$slim->post('/users', Controllers\User::class . ':create');
 
 $slim->run();
