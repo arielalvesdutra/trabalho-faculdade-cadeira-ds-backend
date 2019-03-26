894b34968f98f9c16f46a20cc9b4f465a5f842d5
arielalvesdutra <alves.lsn@gmail.com>
2019-03-26 14:03:17 -0300

    Remove SQL.slq por não ser mais necessário

--- SQL.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users_user_profiles(
-  id_user int (11) not null,
-  id_user_profile int(11) not null,
-  PRIMARY KEY (id_user, id_user_profile),
-  FOREIGN KEY (id_user) REFERENCES users(id),
-  FOREIGN KEY (id_user_profile) REFERENCES user_profiles(id)
-)engine=InnoDB;
