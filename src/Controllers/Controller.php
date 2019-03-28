1e3ff740484feee6c1a5f7e89c0117db2c00a856
arielalvesdutra <alves.lsn@gmail.com>
2019-03-28 16:55:47 -0300

    <feat>(src/Controllers/Controller): adiciona método decodeToken()

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Firebase\JWT\JWT;
+
 /**
  * Class Controller
  * @package App\Controllers
@@ -9,4 +11,46 @@ namespace App\Controllers;
 abstract class Controller
 {
     protected $repository;
+
+    /**
+     * @param string $token
+     *
+     * @return array
+     */
+    protected function decodeToken(string $token)
+    {
+
+        $algorithms = [ 'HS256' ];
+        $secret = $this->getSecret();
+        $tokenWithoutBearer = $this->removeBearerFromToken($token);
+
+        $payload = objectToArray(JWT::decode($tokenWithoutBearer, $secret, $algorithms));
+
+        return $payload;
+    }
+
+    /**
+     * Retorna o secret do JWT configurado no arquivo .env
+     *
+     * @return mixed
+     */
+    private function getSecret()
+    {
+        if (!empty($_ENV['AUTH_SECRET'])) {
+            return $_ENV['AUTH_SECRET'];
+        }
+
+        throw new Exception('Secret não está configurado!');
+    }
+
+    /**
+     * @param string $token
+     *
+     * @return string
+     */
+    private function removeBearerFromToken(string $token)
+    {
+        return str_replace(["bearer ", "Bearer "] , "" , $token);
+    }
+
 }
