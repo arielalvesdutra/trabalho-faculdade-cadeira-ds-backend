0e769cee355188ef2cbb96e716a77971187c56b1
arielalvesdutra <alves.lsn@gmail.com>
2019-03-29 10:12:29 -0300

    Adiciona formatters

---
+++ src/Formatters/HourAdjustmentFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Formatters;
+
+use App\Entities;
+
+class HourAdjustmentFormatter extends Formatter
+{
+    /**
+     * @param $entities Entities\HourAdjustment[]
+     *
+     * @return array
+     */
+    public static function fromEntityArrayToControllerArray($entities)
+    {
+        $controllerArray = [];
+
+        foreach ($entities as $entity) {
+            $controllerArray[] = [
+                'id' => $entity->getId(),
+                'date' => $entity->getDate()->format('Y-m-d'),
+                'entryHour' => $entity->getEntryHour()->format('H:i:s'),
+                'exitHour' => $entity->getExitHour()->format('H:i:s'),
+                'justification' =>  [
+                  'id' => $entity->getJustification()->getId()
+                ],
+                'userId' => $entity->getUserId()
+            ];
+        }
+
+        return $controllerArray;
+    }
+}
