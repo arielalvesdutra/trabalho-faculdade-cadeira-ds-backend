126a83258fd241d23f57f6b87e58c23338935557
arielalvesdutra <alves.lsn@gmail.com>
2019-04-03 10:30:20 -0300

    Adiciona métodos employeeApprovalRequest() e retrieveEmployeeAdjustmentsStatus() no src/Repositories/HourAdjustment.php

--- src/Repositories/HourAdjustment.php
+++ src/Repositories/HourAdjustment.php
@@ -7,14 +7,17 @@ use App\Entities;
 use App\Exceptions\ForbiddenException;
 use App\Exceptions\NotFoundException;
 use App\Factories\Entities\HourAdjustmentEntityFactory;
+use App\Factories\Entities\UserHourAdjustmentStatusFactory;
+use App\Formatters\Formatter;
 use App\Formatters\HourAdjustmentFormatter;
 use App\Models;
+use App\States;
 use OutOfRangeException;
 
 /**
  * Repository de Ajustes de Horas
  *
- * Class HourAdjusment
+ * Class HourAdjustment
  * @package App\Repositories
  */
 class HourAdjustment
@@ -79,6 +82,50 @@ class HourAdjustment
     /**
      * @param array $parameters
      *
+     * @throws NotFoundException
+     */
+    public function employeeApprovalRequest(array $parameters)
+    {
+        $userModel = new Models\User(
+            DefaultDatabaseConnection::getConnection()
+        );
+
+        $userEntity = $userModel->findById($parameters['id_user']);
+
+        $userHoursAdjustmentsStatus = new Models\UserHourAdjustmentStatus(
+            DefaultDatabaseConnection::getConnection()
+        );
+
+        $hoursAdjustmentStatus = new Models\HourAdjustmentStatus(
+            DefaultDatabaseConnection::getConnection()
+        );
+
+        $adjustmentStatusEntity = $hoursAdjustmentStatus->findByCode(
+            (new States\HourAdjustment\WaitingCoordinatorApproval())->getCode());
+
+        try {
+            $userAdjustmentStatusEntity =
+                $userHoursAdjustmentsStatus->findByUserId($userEntity->getId());
+
+            $userAdjustmentStatusEntity->setHourAdjustmentStatus(
+                $adjustmentStatusEntity);
+
+            $userHoursAdjustmentsStatus->update($userAdjustmentStatusEntity);
+
+        } catch (NotFoundException $notFoundException) {
+
+            $userAdjustmentStatusEntity = UserHourAdjustmentStatusFactory::create(
+                $userEntity,
+                $adjustmentStatusEntity
+            );
+
+            $userHoursAdjustmentsStatus->save($userAdjustmentStatusEntity);
+        }
+    }
+
+    /**
+     * @param array $parameters
+     *
      * @return array
      *
      * @throws NotFoundException
@@ -107,6 +154,31 @@ class HourAdjustment
         $hoursAdjustments = $this->model->findAdjustmentsByUserId($userEntity->getId());
 
         return HourAdjustmentFormatter::fromEntityArrayToControllerArray($hoursAdjustments);
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return array
+     *
+     * @throws NotFoundException
+     */
+    public function retrieveEmployeeAdjustmentsStatus(array $parameters)
+    {
+        $userModel = new Models\User(
+            DefaultDatabaseConnection::getConnection()
+        );
+
+        $userEntity = $userModel->findById($parameters['id_user']);
+
+        $userHoursAdjustmentsStatus = new Models\UserHourAdjustmentStatus(
+            DefaultDatabaseConnection::getConnection()
+        );
+
+        $userAdjustmentStatusEntity =
+                $userHoursAdjustmentsStatus->findByUserId($userEntity->getId());
+
+        return Formatter::fromObjectToArray($userAdjustmentStatusEntity->getHourAdjustmentStatus());
     }
 
     /**
